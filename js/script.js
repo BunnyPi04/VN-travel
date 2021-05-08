675b52869ddb69f4c23e2e3d345262a4b14fec67
HangPT <hangpt@ubuntu.ubuntu-domain>
2021-05-08 18:15:41 +0700

    respon home page

--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
-// //Allows bootstrap carousels to display 3 items per page rather than just one
-// $('.carousel.carousel-multi .item').each(function () {
-//     var next = $(this).next();
-//     if (!next.length) {
-//         next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().attr("aria-hidden", "true").appendTo($(this));
+var scrollTop = $('.back-to-top');
+$(window).scroll(function() {
+    var topPos = $(this).scrollTop();
 
-//     if (next.next().length > 0) {
-//         next.next().children(':first-child').clone().attr("aria-hidden", "true").appendTo($(this));
-//     } else {
-//         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-//     }
-//     console.log('f')
-// });
+    // if user scrolls down - show scroll to top button
+    if (topPos > 100) {
+        $(scrollTop).css('opacity', '1');
+    } else {
+        $(scrollTop).css('opacity', '0');
+    }
+}); // scroll END
+
+//animate hover scrolltop button
+$(scrollTop).mouseenter(function() {
+    $('.to-top-arrow').animate({
+        marginBottom: '+=30px', //moves up
+    });
+}).mouseleave(function() {
+    $('.to-top-arrow').animate({
+        marginBottom: '-=30px', //moves down
+    });
+});
+
+//Click event to scroll to top
+$(scrollTop).click(function() {
+    $('html, body').animate({
+        scrollTop: 0,
+    }, 1500);
+
+    return false;
+}); // click() scroll top END
+
+$('.home-slick').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    dotsClass: 'slick-dots',
+    prevArrow: `<a class="slick-arrow__left" href="#tesmonial-slide" >
+                    <span class="nsy-icon nsy-slide-arrow-left"></span>
+                </a>`,
+    nextArrow: `<a class="slick-arrow__right" href="#tesmonial-slide">
+                    <span class="nsy-icon nsy-slide-arrow-right"></span>
+                </a>`,
+    responsive: [
+        {
+        breakpoint: 769,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false
+        }
+    }
+  ]
+});
